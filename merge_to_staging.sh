0b79636249cd1d1b75669ab21c57b6fcb554affd
Nicolas <nicolusmaluleke@gmail.com>
2023-08-15 12:51:31 +0200

    Allow user to override staging branch name as argument by providng the -n flag followed by the name of the branch

--- merge_to_staging.sh
+++ merge_to_staging.sh
@@ -2,22 +2,28 @@
 
 # Function to print script usage
 function print_usage {
-    echo "Usage: $0 [-p] [-f]"
+    echo "Usage: $0 [-p] [-f] [-n <staging_branch_name>]"
     echo "    -p: Push the changes to the remote staging branch"
     echo "    -f: Override staging with the current branch"
+    echo "    -n <staging_branch_name>: Specify a name for the staging branch"
     exit 1
 }
 
 # Parse the arguments
 push_to_remote=false
 override_staging=false
-while getopts ":pf" opt; do
+staging_branch_name="staging"  # Default staging branch name
+
+while getopts ":pfn:" opt; do
     case "$opt" in
         p)
             push_to_remote=true
             ;;
         f)
             override_staging=true
+            ;;
+        n)
+            staging_branch_name="$OPTARG"
             ;;
         \?)
             echo "Invalid option: -$OPTARG"
@@ -26,30 +32,33 @@ while getopts ":pf" opt; do
     esac
 done
 
+# Check if both -p and -f flags are provided (which is not allowed)
+if [ "$push_to_remote" = true ] && [ "$override_staging" = true ]; then
+    echo "Error: The -p and -f options cannot be used together."
+    print_usage
+fi
+
 # Get the current branch name
 current_branch=$(git symbolic-ref --short HEAD)
 
-# Define the staging branch name
-staging_branch="staging-nick"
-
 # Check if the staging branch exists
-if ! git show-ref --quiet refs/heads/"$staging_branch"; then
-    echo "Error: The '$staging_branch' branch does not exist in the repository."
+if ! git show-ref --quiet refs/heads/"$staging_branch_name"; then
+    echo "Error: The '$staging_branch_name' branch does not exist in the repository."
     exit 1
 fi
 
 # Override staging with the current branch if -f flag is provided
 if [ "$override_staging" = true ]; then
-    git checkout -B "$staging_branch" "$current_branch"
+    git checkout -B "$staging_branch_name" "$current_branch"
     echo "Staging branch overridden with the current branch: '$current_branch'."
 else
     # Merge the current branch into the staging branch
-    git checkout "$staging_branch"
+    git checkout "$staging_branch_name"
     git merge "$current_branch"
 
     # Check if merge was successful
     if [ $? -eq 0 ]; then
-        echo "Merge successful: '$current_branch' merged into '$staging_branch'."
+        echo "Merge successful: '$current_branch' merged into '$staging_branch_name'."
     else
         echo "Merge failed. Please resolve conflicts and try again."
         exit 1
@@ -58,11 +67,11 @@ fi
 
 # Optionally push the changes to the remote staging branch
 if [ "$push_to_remote" = true ]; then
-    git push origin "$staging_branch"
+    git push origin "$staging_branch_name"
     if [ $? -eq 0 ]; then
-        echo "Changes pushed to remote '$staging_branch'."
+        echo "Changes pushed to remote '$staging_branch_name'."
     else
-        echo "Error: Failed to push changes to remote '$staging_branch'."
+        echo "Error: Failed to push changes to remote '$staging_branch_name'."
         exit 1
     fi
 fi
